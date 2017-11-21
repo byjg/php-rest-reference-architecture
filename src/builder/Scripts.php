9ce0ff6aeb7bf57be1c4e60024afdf53c2eef101
Joao Gilberto Magalhães <joao@byjg.com.br>
2017-11-21 16:50:42 -0200

    Fix path in runGenRestDocs()

--- src/builder/Scripts.php
+++ src/builder/Scripts.php
@@ -95,7 +95,7 @@ class Scripts extends _Lib
     {
         $docPath = $this->workdir . '/web/docs/';
         $this->liveExecuteCommand(
-            $this->workdir . "vendor/bin/swagger --output \"$docPath\" --exclude vendor,docker,fw"
+            $this->workdir . "/vendor/bin/swagger --output \"$docPath\" --exclude vendor,docker,fw"
         );
 
         $docs = file_get_contents("$docPath/swagger.json");
