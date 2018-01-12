dfb6d719656054f5b60139ae9cc6ab3db1280825
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-01-11 18:54:02 -0600

    Changed the RoutePattern to their factories.
    (cherry picked from commit a3f5f477c7e2ffd64a602ac2a2c412e7e3da9a01)

--- config/config-dev.php
+++ config/config-dev.php
@@ -9,22 +9,45 @@ return [
 
     'HOST' => 'localhost',
 
-    'ROUTE_CLASSMAP' => [
-        'login' => 'RestTemplate.Rest.Login',
-        'sample' => 'RestTemplate.Rest.Sample',
-        'sampleprotected' => 'RestTemplate.Rest.SampleProtected',
-    ],
     'ROUTE_PATH' => [
-        new RoutePattern('POST', '/{module:login}', JsonHandler::class),
+        RoutePattern::post(
+            '/login',
+            'post',
+            \RestTemplate\Rest\Login::class
+        ),
     ],
     'ROUTE_PATH_EXTRA' => [
         // Specific for this Environment
-        new RoutePattern('GET', '/{module:sample}/{action:ping}', JsonHandler::class),
-        new RoutePattern('GET', '/{module:sample}/{action:dummy}/{field}', JsonHandler::class),
-        new RoutePattern('POST', '/{module:sample}/{action:dummy}', JsonHandler::class),
-        new RoutePattern('GET', '/{module:sampleprotected}/{action:pingadm}', JsonHandler::class),
-        new RoutePattern('GET', '/{module:sampleprotected}/{action:ping}', JsonHandler::class),
-        new RoutePattern('POST', '/{module:sampleprotected}/{action:adduser}', JsonHandler::class),
+        RoutePattern::get(
+            '/sample/ping',
+            'getPing',
+            \RestTemplate\Rest\Sample::class
+        ),
+        RoutePattern::get(
+            '/sample/dummy/{field}',
+            'getDummy',
+            \RestTemplate\Rest\Sample::class
+        ),
+        RoutePattern::post(
+            '/sample/dummy',
+            'postDummy',
+            \RestTemplate\Rest\Sample::class
+        ),
+        RoutePattern::get(
+            '/sampleprotected/pingadm',
+            'getPingadm',
+            \RestTemplate\Rest\SampleProtected::class
+        ),
+        RoutePattern::get(
+            '/sampleprotected/ping',
+            'getPing',
+            \RestTemplate\Rest\SampleProtected::class
+        ),
+        RoutePattern::post(
+            '/sampleprotected/adduser',
+            'postAdduser',
+            \RestTemplate\Rest\SampleProtected::class
+        ),
     ],
 
     'JWT_SERVER' => "localhost",
