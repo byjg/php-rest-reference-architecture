c186afa45fb522b9768ad44e3ce1a9bb34870bb7
Joao Gilberto Magalhaes <joao@byjg.com.br>
2023-02-14 12:04:28 -0600

    Config Adjust

--- config/config-test.php
+++ config/config-test.php
@@ -4,9 +4,6 @@ use ByJG\Config\DependencyInjection as DI;
 use ByJG\Util\JwtKeySecret;
 
 return [
-
-    'DBDRIVER_CONNECTION' => 'mysql://root:mysqlp455w0rd@mysql-container/mydb',
-
     JwtKeySecret::class => DI::bind(JwtKeySecret::class)
         ->withConstructorArgs(['jwt_super_secret_key'])
         ->toSingleton(),
