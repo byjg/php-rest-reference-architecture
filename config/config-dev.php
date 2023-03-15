bdba9f4b70bab5a0de3707ea96ccaff35a54d992
Joao Gilberto Magalhaes <joao@byjg.com.br>
2023-03-14 20:44:47 -0500

    Added Code Generator

--- config/config-dev.php
+++ config/config-dev.php
@@ -108,6 +108,7 @@ return [
             ]
         )
         ->withMethodCall("markPropertyAsReadOnly", ["uuid"])
+        ->withMethodCall("markPropertyAsReadOnly", ["updated"])
         ->withMethodCall("defineGenerateKeyClosure", 
             [
                 function () {
