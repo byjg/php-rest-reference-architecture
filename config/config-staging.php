02e1ad66fb878d5464973d91e676b1939722f2d4
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-03-18 21:54:52 -0500

    More adjustents

--- config/config-staging.php
+++ config/config-staging.php
@@ -3,13 +3,14 @@
 use ByJG\Cache\Psr16\BaseCacheEngine;
 use ByJG\Cache\Psr16\FileSystemCacheEngine;
 use ByJG\Config\DependencyInjection as DI;
+use ByJG\JwtWrapper\JwtHashHmacSecret;
 use ByJG\JwtWrapper\JwtKeyInterface;
 
 return [
 
     BaseCacheEngine::class => DI::bind(FileSystemCacheEngine::class)->toSingleton(),
 
-    JwtKeyInterface::class => DI::bind(\ByJG\JwtWrapper\JwtHashHmacSecret::class)
+    JwtKeyInterface::class => DI::bind(JwtHashHmacSecret::class)
         ->withConstructorArgs(['jwt_super_secret_key'])
         ->toSingleton(),
 
--- config/config-test.php
+++ config/config-test.php
@@ -1,10 +1,11 @@
 <?php
 
 use ByJG\Config\DependencyInjection as DI;
+use ByJG\JwtWrapper\JwtHashHmacSecret;
 use ByJG\JwtWrapper\JwtKeyInterface;
 
 return [
-    JwtKeyInterface::class => DI::bind(\ByJG\JwtWrapper\JwtHashHmacSecret::class)
+    JwtKeyInterface::class => DI::bind(JwtHashHmacSecret::class)
         ->withConstructorArgs(['jwt_super_secret_key'])
         ->toSingleton(),
 ];
